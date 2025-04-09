502e154a1116ec9b67244e700325238761d492ae
cmdyc <PB23111596@mail.ustc.edu.cn>
2025-04-09 11:21:00 +0800

    The pipe function was implemented

--- lab2/shell.cpp
+++ lab2/shell.cpp
@@ -35,7 +35,7 @@ int main() {
     std::vector<std::string> args = split(cmd, " ");
 
     // 按" | "分割命令为子命令
-    std::vector<std::string> comds = split(cmd, " | ");
+    std::vector<std::string> scomds = split(cmd, " | ");
 
     // 没有可处理的命令
     if (args.empty()) {
@@ -129,25 +129,174 @@ int main() {
     // 子进程返回 0
     // 失败时返回 -1
     pid_t pid = fork();
+    /*
+    if (pid < 0) {
+      // fork() 失败
+      perror("fork failed");
+      continue;
+    } else if (pid == 0) {
+      // 子进程
+      if (scomds.size() > 1) {
+        // 管道
+        int fd[scomds.size()-1][2];
+        // 为scomds中的所有子命令创建进程，并通过管道按顺序连接起来，实现进程间通信，之后执行这些命令
+        for (int i = 0; i < scomds.size(); i++) {
+          // 创建管道
+          if (pipe(fd[i]) == -1) {
+            perror("pipe failed");
+            continue;
+          }
+          // 将子命令分割为命令与参数的格式
+          std::vector<std::string> scomd_args = split(scomds[i], " ");
+          // fork 子进程
+          pid_t cpid = fork();
+          if (cpid < 0) {
+            perror("fork failed");
+            continue;
+          } else {
+            // 将子命令的输出与输入重定向到管道
+            // 由于是子进程进行的fork()，所以这里的pid如果正确生成的话均为0
 
-    // std::vector<std::string> 转 char **
-    char *arg_ptrs[args.size() + 1];
-    for (auto i = 0; i < args.size(); i++) {
-      arg_ptrs[i] = &args[i][0];
+            // 将scomds中除第一个子进程外所有的子进程的输入重定向到管道
+            if (i > 0) {
+              // 关闭管道的写端
+              close(fd[i-1][1]);
+              // 将标准输入重定向到管道的读端
+              dup2(fd[i-1][0], STDIN_FILENO);
+              // 关闭管道的读端
+              close(fd[i-1][0]);
+            }
+            // 将scomds中除最后一个子进程外所有的子进程的输出重定向到管道
+            if (i < scomds.size()-1) {
+              // 关闭管道的读端
+              close(fd[i][0]);
+              // 将标准输出重定向到管道的写端
+              dup2(fd[i][1], STDOUT_FILENO);
+              // 关闭管道的写端
+              close(fd[i][1]);
+            } 
+          }
+          // std::vector<std::string> 转 char **
+          char *scomd_arg_ptrs[scomd_args.size() + 1];
+          for (auto i = 0; i < scomd_args.size(); i++) {
+            scomd_arg_ptrs[i] = &scomd_args[i][0];
+          }
+          // execvp 系列的 argv 需要以 nullptr 结尾
+          scomd_arg_ptrs[scomd_args.size()] = nullptr;
+
+          // 执行子命令
+          // execvp 会完全更换子进程接下来的代码，所以正常情况下 execvp 之后这里的代码就没意义了
+          // 如果 execvp 之后的代码被运行了，那就是 execvp 出问题了
+          execvp(scomd_args[0].c_str(), scomd_arg_ptrs);
+
+          // 所以这里直接报错
+          perror("execvp failed");
+          exit(255);
+        }
+      } else {
+        // 只有一个子命令，即没有使用管道通信
+        // std::vector<std::string> 转 char **
+        char *arg_ptrs[args.size() + 1];
+        for (auto i = 0; i < args.size(); i++) {
+          arg_ptrs[i] = &args[i][0];
+        }
+        // execvp 系列的 argv 需要以 nullptr 结尾
+        arg_ptrs[args.size()] = nullptr;
+
+        // 执行命令
+        // execvp 会完全更换子进程接下来的代码，所以正常情况下 execvp 之后这里的代码就没意义了
+        // 如果 execvp 之后的代码被运行了，那就是 execvp 出问题了
+        execvp(args[0].c_str(), arg_ptrs);
+
+        // 所以这里直接报错
+        perror("execvp failed");
+        exit(255);
+      }
     }
-    // execvp 系列的 argv 需要以 nullptr 结尾
-    arg_ptrs[args.size()] = nullptr;
+    */
+if (pid < 0) {
+    perror("fork failed");
+    continue;
+} else if (pid == 0) {
+    // 子进程
+    if (scomds.size() > 1) {
+        int pipe_count = scomds.size() - 1;
+        int fd[scomds.size() - 1][2];
+        for (int i = 0; i < pipe_count; ++i) {
+            if (pipe(fd[i]) == -1) {
+                perror("pipe failed");
+                continue;;
+            }
+        }
 
-    if (pid == 0) {
-      // 这里只有子进程才会进入
-      // execvp 会完全更换子进程接下来的代码，所以正常情况下 execvp 之后这里的代码就没意义了
-      // 如果 execvp 之后的代码被运行了，那就是 execvp 出问题了
-      execvp(args[0].c_str(), arg_ptrs);
+        std::vector<pid_t> child_pids;
 
-      // 所以这里直接报错
-      perror("execvp failed");
-      exit(255);
+        for (int i = 0; i < scomds.size(); ++i) {
+            pid_t cpid = fork();
+            if (cpid < 0) {
+                perror("fork failed");
+                continue;
+            } else if (cpid == 0) {
+                // 输入重定向
+                if (i > 0) {
+                    dup2(fd[i-1][0], STDIN_FILENO);
+                    close(fd[i-1][0]);
+                }
+                // 输出重定向
+                if (i < pipe_count) {
+                    dup2(fd[i][1], STDOUT_FILENO);
+                    close(fd[i][1]);
+                }
+                // 关闭所有管道描述符
+                for (int j = 0; j < pipe_count; ++j) {
+                    close(fd[j][0]);
+                    close(fd[j][1]);
+                }
+                // 执行子命令
+                std::vector<std::string> scomd_args = split(scomds[i], " "); // 存储分割后的子命令参数
+                std::vector<char*> argv; // 存储转换为C风格字符串的指针
+                // 遍历 scomd_args 中的每个元素, auto& 声明引用以避免拷贝
+                for (auto& arg : scomd_args) {
+                    argv.push_back(const_cast<char*>(arg.c_str())); // execvp 函数要求第一个参数是 const char*, 强制移除
+                }
+                argv.push_back(nullptr); // execvp 系列的 argv 需要以 nullptr 结尾
+
+                // execvp 会完全更换子进程接下来的代码，所以正常情况下 execvp 之后这里的代码就没意义了
+                // 如果 execvp 之后的代码被运行了，那就是 execvp 出问题了
+                execvp(argv[0], argv.data());
+                // 所以这里直接报错
+                perror("execvp failed");
+                exit(255);
+            } else {
+                child_pids.push_back(cpid);
+            }
+        }
+
+        // 父进程关闭所有管道
+        for (int i = 0; i < pipe_count; ++i) {
+            close(fd[i][0]);
+            close(fd[i][1]);
+        }
+
+        // 等待所有子进程
+        for (pid_t pid : child_pids) {
+            waitpid(pid, nullptr, 0);
+        }
+
+        continue;
+    } else {
+        // 处理单个命令
+        std::vector<std::string> scomd_args = split(scomds[0], " ");
+        std::vector<char*> argv;
+        for (auto& arg : scomd_args) {
+            argv.push_back(const_cast<char*>(arg.c_str()));
+        }
+        argv.push_back(nullptr);
+        execvp(argv[0], argv.data());
+        perror("execvp failed");
+        exit(255);
     }
+}
 
     // 这里只有父进程（原进程）才会进入
     // 父进程调用 wait(nullptr) 阻塞等待子进程结束，并回收其资源
